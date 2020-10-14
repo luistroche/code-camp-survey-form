4bfe61c9f944122dcd7fafa56a2b1d1b4c88fea9
Luis <luistroche@gmail.com>
2020-10-13 22:41:19 -0400

    document.getElementById switch classes

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -1,1 +1,7 @@
 // Freecodecamp basic survey form javascript
+
+let starImage = '<img src="images/star-50.png" class="star"></div>';
+
+changing star classes:
+// document.getElementById("star22").className = "star-reverse"; add star reverse as class
+// document.getElementById("star22").className = "none"; set display: none to Element.
